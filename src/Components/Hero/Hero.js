6295fd14ed81af475147001bffef0f2cce22a2db
alexfit4 <alex.ashtiany@gmail.com>
2021-09-18 17:41:04 -0400

    starting page

---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import video from '../Assets/IMG_0096.mp4'
+import { Player } from 'video-react';
+import ReactPlayer from 'react-player';
+export default function Hero() {
+    return (
+			<div>
+				<div className='container mx-auto'>
+					<video className="ml-28" controls autostart autoPlay src={video} type='video/mp4' width={1200} />
+				</div>
+			</div>
+		);
+}
